6ef461d3b10f33a18f9da624984427c5a460bf88
pertempcent <74292699+pertempcent@users.noreply.github.com>
2020-11-13 16:36:39 +0530

    Update crypto-test.js

--- crypto-test.js
+++ crypto-test.js
@@ -1,47 +1,50 @@
 function encrypt() {
 	var str = document.getElementById("plain-text").value;
-	document.getElementById("encrypted-message").value = caesarEncrypt(str);
+	var shift = parseInt(document.getElementById("shift").value,10);
+	document.getElementById("encrypted-message").value = caesarEncrypt(str,shift);
 }
 
 function decrypt() {
 	var str = document.getElementById("encrypted-text").value;
-	document.getElementById("decrypted-plain-text").value = caesarDecrypt(str);
+	var shift = parseInt(document.getElementById("shift").value,10);
+	document.getElementById("decrypted-plain-text").value = caesarDecrypt(str,shift);
 }
 
 
-function caesarEncrypt(message) { // add a key to function
+function caesarEncrypt(message, shift) {
+	console.log(shift); 
 	var num = [];
 		if (message.charCodeAt(i) >=97 && message.charCodeAt(i) <=122)
 			for(var i = 0; i < message.length; i++)
-			num[i] = (message.charCodeAt(i) + 13 - 97)% 40 + 97;
+			num[i] = (message.charCodeAt(i) + shift - 97)%(26+shift) + 97;
 		else if (message.charCodeAt(i) >=65 && message.charCodeAt(i) <=90)
 			for(var i = 0; i < message.length; i++)
-			num[i] = (message.charCodeAt(i) + 13 - 65)% 40 + 65;
+			num[i] = (message.charCodeAt(i) + shift - 65)%(26+shift) + 65;
 	return String.fromCharCode( ...num );
 }
 
-function caesarDecrypt(message) {
+function caesarDecrypt(message, shift) {
 	var num = [];
 		if (message.charCodeAt(i) >=97 && message.charCodeAt(i) <=122)
 			for(var i = 0; i < message.length; i++)
-			num[i] = (message.charCodeAt(i) - 13 - 97)% 40 + 97;
+			num[i] = (message.charCodeAt(i) - shift - 97)%(26+shift) + 97;
 		else if (message.charCodeAt(i) >=65 && message.charCodeAt(i) <=90)
 			for(var i = 0; i < message.length; i++)
-			num[i] = (message.charCodeAt(i) - 13 - 65)% 40 + 65;
+			num[i] = (message.charCodeAt(i) - shift - 65)%(26+shift) + 65;
 	return String.fromCharCode( ...num );
 }
 
 function copyOne() {
 const enStr = document.getElementById("encrypted-message");
 enStr.focus(); 
-enStr.select(); 
+enStr.select(); // to copy you need to select first
 document.execCommand("copy");
 }
 
 function copyTwo() {
 const enStr = document.getElementById("decrypted-plain-text");
 enStr.focus(); 
-enStr.select(); 
+enStr.select(); // to copy you need to select first
 document.execCommand("copy");
 }
 
